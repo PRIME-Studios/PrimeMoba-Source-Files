d9b4f558bead5cd8a775b79ad569d610e5b0a361
HawkSP <elliott.mitchell10@gmail.com>
2017-08-17 20:06:39 +0100

    PrimeMoba 0.01
    This is the first version of PrimeMoba

---
+++ Assets/Scripts/GUIControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIControl : MonoBehaviour {
+
+	void Update () {
+		if (Character.victoriousTeam == '0') {
+			GetComponent<GUIText>().text = Character.killsA + " x " + Character.killsB;
+		}
+		else {
+			if (Character.victoriousTeam == 'v')
+				GetComponent<GUIText>().text = Character.killsA + " x " + Character.killsB +"\nTeam B Won";
+			else {
+				GetComponent<GUIText>().text = Character.killsA + " x " + Character.killsB +"\nTeam A Won";
+			}
+		}
+	}
+}
